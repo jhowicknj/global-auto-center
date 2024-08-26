b0d6536997d2cf74ddba0283ebc4c2a7fe126a72
jhowicknj <158176583+jhowicknj@users.noreply.github.com>
2024-08-26 13:17:15 -0300

    site global auto center

---
+++ script.js
@@ -0,0 +1,9 @@
+// Rolagem suave para links de navegação
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
